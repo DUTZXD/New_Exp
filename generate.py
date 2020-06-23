dc31300ba9c408a799182b6361661011230acfa3
DUTZXD <951383631@qq.com>
2020-06-23 14:21:56 +0800

    1

--- generate.py
+++ generate.py
@@ -29,7 +29,7 @@ def gen_test_txt(txt_path, img_dir):
             for i in range(len(img_list)):
                 if not img_list[i].endswith('png'):  # 若不是png文件，跳过
                     continue
-                label_path = os.path.join("../LOLdataset/eval15/high/", img_list[i])
+                label_path = os.path.join("./LOLdataset/eval15/high/", img_list[i])
                 img_path = os.path.join("../LOLdataset/eval15/low/", img_list[i])
                 line = img_path + '***' + label_path + '\n'
                 f.write(line)
@@ -37,6 +37,20 @@ def gen_test_txt(txt_path, img_dir):
     f.close()
 
 
+def gen_result_txt(txt_path, img_dir):
+    f = open(txt_path, 'w')
+    res_list = os.listdir(img_dir)
+    res_list.sort(key=lambda x: int(x[:-4]))
+    label_list = os.listdir("../fivek_dataset/expert")
+    # label_list.sort(key=lambda x: int(x[:-4]))
+    for i in range(len(res_list)):
+        res_path = os.path.join("result", res_list[i])
+        label_path = os.path.join("../fivek_dataset/expert", label_list[i])
+        line = res_path + "***" + label_path
+        f.write(line + "\n")
+    f.close()
+
+
 if __name__ == '__main__':
     # gen_train_txt("./lol_train.txt", "../LOLdataset/our485")
-    gen_test_txt("./lol_test.txt", "../LOLdataset/eval15")
+    gen_result_txt("pre_result.txt", "./result")
--- run_v2.py
+++ run_v2.py
@@ -6,8 +6,8 @@ import torch.nn.functional as F
 import kornia
 import tv_loss
 import my_loss
-from scipy.ndimage import gaussian_filter
-import Gaussian
+# from scipy.ndimage import gaussian_filter
+# import Gaussian
 import torchvision.transforms.functional as tf
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
@@ -17,7 +17,7 @@ from mydataset import MyData
 
 batch_size = 16
 epoches = 1000
-lr = 0.0001
+lr = 0.001
 
 
 # 定义残差块结构
@@ -47,49 +47,39 @@ class Net(nn.Module):
         self.layer0 = self.make_layer(3, 3, 5)
         self.layer1 = self.make_layer(3, 3, 3)
         self.layer2 = self.make_layer(3, 3, 1)
-        # self.smoothing0 = Gaussian.GaussianSmoothing(3, 7, 1.3)
-        # self.smoothing1 = Gaussian.GaussianSmoothing(3, 5, 1.1)
-        # self.smoothing2 = Gaussian.GaussianSmoothing(3, 3, 0.8)
-        # self.gaussian = Gaussian.GaussianBlurConv()
 
     def make_layer(self, inChannel, outChannel, kernel):
-        layer = [ResidualBlock(inChannel, outChannel, kernel_size=kernel, stride=1, padding=int((kernel-1)/2)),
-                 ResidualBlock(outChannel, outChannel, kernel_size=kernel, stride=1, padding=int((kernel-1)/2)),
-                 ResidualBlock(outChannel, outChannel, kernel_size=kernel, stride=1, padding=int((kernel-1)/2))]
-
-        # layer = [ResidualBlock(inChannel, outChannel, kernel_size=5, stride=1, padding=2),
-        #          ResidualBlock(outChannel, outChannel, kernel_size=3, stride=1, padding=1),
-        #          ResidualBlock(outChannel, outChannel, kernel_size=1, stride=1, padding=0)]
+        layer = [ResidualBlock(inChannel, outChannel, kernel_size=kernel, stride=1, padding=int((kernel - 1) / 2)),
+                 ResidualBlock(outChannel, outChannel, kernel_size=kernel, stride=1, padding=int((kernel - 1) / 2)),
+                 ResidualBlock(outChannel, outChannel, kernel_size=kernel, stride=1, padding=int((kernel - 1) / 2))]
         return nn.Sequential(*layer)
 
     def forward(self, x):
         # 先进行高斯滤波及下采样
-        # img0 = self.gaussian(x)
-        # img0 = self.smoothing0(x)
         img0 = kornia.gaussian_blur2d(x, (5, 5), (1.1, 1.1))
 
         img1 = F.interpolate(img0, scale_factor=0.5)
-        # img1 = self.gaussian(img1)
-        # img1 = g_difference(img1, 5)
         img1 = kornia.gaussian_blur2d(img1, (3, 3), (0.8, 0.8))
 
         img2 = F.interpolate(img1, scale_factor=0.5)
-        # img2 = self.gaussian(img2)
-        # img2 = g_difference(img2, 3)
         img2 = kornia.gaussian_blur2d(img2, (1, 1), (0.5, 0.5))
 
-        img0_log = -torch.log(img0 + 0.0001)
-        img1_log = -torch.log(img1 + 0.0001)
-        img2_log = -torch.log(img2 + 0.0001)
+        x0 = self.layer0(img0)
+        x1 = self.layer1(img1)
+        x2 = self.layer2(img2)
+        # save_image(x2, './tmp3/illu2.png')
+        # save_image(x1, './tmp3/illu1.png')
+        # save_image(x0, './tmp3/illu0.png')
 
-        x2 = -self.layer2(img2_log)
-        x1 = -self.layer1(img1_log)
-        x0 = -self.layer0(img0_log)
+        x0_log = torch.log(x0 + 1e-10)
+        x1_log = torch.log(x1 + 1e-10)
+        x2_log = torch.log(x2 + 1e-10)
 
-        x2 = F.interpolate(x2, size=[img0_log.shape[2], img0_log.shape[3]], mode='nearest')
-        x1 = F.interpolate(x1, size=[img0_log.shape[2], img0_log.shape[3]], mode='nearest')
-        illumination = x0 + x1 + x2
-        return illumination
+        x2_log = F.interpolate(x2_log, size=[x0_log.shape[2], x0_log.shape[3]], mode='nearest')
+        x1_log = F.interpolate(x1_log, size=[x0_log.shape[2], x0_log.shape[3]], mode='nearest')
+
+        illumination_log = (x0_log + x1_log + x2_log)/3.0
+        return illumination_log
 
 
 # 保存结果
@@ -104,26 +94,26 @@ def save_image(input_tensor: torch.Tensor, filename):
 def train(model, device, train_loader, optimizer, epoch):
     model.train()
     criterion1 = nn.MSELoss()
-    # criterion1 = SSIM()
     criterion2 = tv_loss.TVLoss()
     criterion3 = my_loss.MyLoss()
     running_loss = 0.0
     for batch_index, train_data in enumerate(train_loader):
         inputs, labels = train_data
         inputs, labels = inputs.to(device), labels.to(device)
-        input_log = torch.log(inputs + 0.0001)
+        input_log = torch.log(inputs + 1e-10)
         illus = model(inputs)
         outputs = torch.exp(input_log - illus)
-        loss = criterion1(outputs, labels) + criterion2(illus) + criterion3(outputs, labels)
-        # loss = criterion1(outputs, labels) + criterion2(illus)
+        # loss = criterion1(outputs, labels) + criterion2(illus) + criterion3(outputs, labels)
+        loss = criterion1(outputs, labels) + criterion2(illus)
         running_loss += loss.item()
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        print('batch_index:', batch_index, 'Epoch: %d Current train loss: %4f' % (epoch, running_loss/(batch_index+1)))
-    torch.save(model.state_dict(), 'net_paramstmp.pkl')
+        print('batch_index:', batch_index,
+              'Epoch: %d Current train loss: %4f' % (epoch, running_loss / (batch_index + 1)))
+    torch.save(model.state_dict(), 'net_params_v2.pkl')
     print('loss: ', running_loss)
-    with open('loss.txt', 'a') as f:
+    with open('loss_v2.txt', 'a') as f:
         f.write(str(running_loss) + "\n")
     print('第', epoch, '轮训练结束，网络参数更新')
 
@@ -137,11 +127,14 @@ def test(model, device, test_loader, epoch):
         for index, test_data in enumerate(test_loader):
             inputs, labels = test_data
             inputs, labels = inputs.to(device), labels.to(device)
-            input_log = torch.log(inputs + 0.0001)
+            input_log = torch.log(inputs + 1e-10)
             illus = model(inputs)
             outputs = torch.exp(input_log - illus)
-            save_image(outputs, './tmp2/' + str(index) + '.png')
+            # print(input_log)
+            save_image(outputs, './tmp3/' + str(index) + '.png')
             test_loss += criterion(outputs, labels).item()
+            # if(index==0):
+            #     break
     test_loss /= len(test_loader.dataset)
     print('Epoch: %d Current test loss: ', epoch, test_loss)
 
@@ -149,7 +142,7 @@ def test(model, device, test_loader, epoch):
 def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = Net().to(device)
-    model.load_state_dict(torch.load('net_paramstmp.pkl'))
+    model.load_state_dict(torch.load('net_params_v2.pkl'))
 
     train_data = MyData('./train.txt', transform=transforms.Compose([transforms.ToTensor()]))
     train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
@@ -158,10 +151,8 @@ def main():
     test_loader = DataLoader(dataset=test_data, batch_size=1, shuffle=False)
 
     optimizer = optim.Adam(model.parameters(), lr=lr)
-    # optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
-    # optimizer = optim.RMSprop(model.parameters(), lr=lr, momentum=0.9)
-    # for epoch in range(epoches):
-    #     train(model, device, train_loader, optimizer, epoch)
+    for epoch in range(epoches):
+        train(model, device, train_loader, optimizer, epoch)
     test(model, device, test_loader, 1)
 
 
